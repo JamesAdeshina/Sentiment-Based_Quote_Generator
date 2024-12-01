50764349fcf888fe1011ee1f6a9baa439213e2ec
James <adeshina.james@bowen.edu.ng>
2024-12-01 23:57:30 +0000

    General update

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import pandas as pd
 quotes = pd.read_csv('data/quote_stash.csv')
 
-# quotes = quotes[['quote','author']]
-
-# quotes.head()
-
-# print(quotes.head())
+quotes = quotes[['quote','author']]
+#
+quotes.head()
+#
+print(quotes.head())
 
 
 
