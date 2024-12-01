e8a88345ba615df3fbf37e5fb62b0c7936cbc3b4
James <adeshina.james@bowen.edu.ng>
2024-12-01 23:49:53 +0000

    working on data_resampled.ipynb file

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import pandas as pd
 quotes = pd.read_csv('data/quote_stash.csv')
 
-quotes = quotes[['quote','author']]
+# quotes = quotes[['quote','author']]
 
-quotes.head()
+# quotes.head()
 
-print(quotes.head())
+# print(quotes.head())
 
 
 
